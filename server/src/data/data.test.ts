7c2436df02ee7cb24b2ce5ca98237b074e68d16a
alihejazi <ahejazi1999@gmail.com>
2024-02-06 21:03:17 +0000

    Test additions

--- server/src/data/data.test.ts
+++ server/src/data/data.test.ts
@@ -49,4 +49,40 @@ describe("Storage", () => {
       expect(db.get(username,"a")).toEqual(db1.get(username, "a"));
     }
   );
+
+  it.concurrent(
+    "should add same item to DIFFERENT users separately",
+    async () => {
+      const db = InventoryDB();
+      const username1 = "testuser1";
+      const username2 = "testuser2";
+
+      await db.set(username1, "a", [1]);
+      await db.set(username2, "a", [2, 2]);
+
+      expect(db.get(username1,"a")).toEqual(1);
+      expect(db.get(username2,"a")).toEqual(4);
+    }
+  );
+
+  it.concurrent(
+    "should add same item to DIFFERENT users separately, and calculate collective quantities correctly",
+    async () => {
+      const db = InventoryDB();
+      const username1 = "testuser1";
+      const username2 = "testuser2";
+
+      await db.set(username1, "a", [1]);
+      await db.set(username2, "a", [2, 2]);
+
+      await db.set(username1, "a", [3, 8]);
+      await db.set(username2, "a", [1, 5]);
+
+      await db.set(username1, "a", [2, 9]);
+      await db.set(username2, "a", [7, 4]);
+
+      expect(db.get(username1,"a")).toEqual(1 + 3 + 8 + 2 + 9);
+      expect(db.get(username2,"a")).toEqual(2 + 2 + 1 + 5 + 7 + 4);
+    }
+  );
 });

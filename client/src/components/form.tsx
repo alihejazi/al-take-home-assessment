5bb97563a9cf3b654b0b39d37b509735c0718566
alihejazi <ahejazi1999@gmail.com>
2024-02-06 17:36:30 +0000

    Bit of refactoring

--- client/src/components/form.tsx
+++ client/src/components/form.tsx
@@ -8,66 +8,76 @@ import {
   Input,
   Spacer,
 } from "@chakra-ui/react";
-import { Field, FieldInputProps, Form, Formik } from "formik";
+import { Field, FieldInputProps, Form, Formik, FormikHelpers } from "formik";
 import { useContext } from "react";
 
 export default function InventoryForm() {
   const { dispatch } = useContext(InventoryContext);
 
-  const users = [
+  type User = {
+    username: string;
+    firstName: string;
+    lastName: string;
+  }
+
+  type Values = { name?: string; quantities?: string; username?: string };
+
+  const users: User[] = [
     {
       username: 'jdoe',
-      name: 'John',
-      surname: 'Doe',
+      firstName: 'John',
+      lastName: 'Doe',
     },
     {
       username: 'ssmith',
-      name: 'Sarah',
-      surname: 'Smith',
+      firstName: 'Sarah',
+      lastName: 'Smith',
     },
     {
       username: 'asimons',
-      name: 'Amanda',
-      surname: 'Simons',
+      firstName: 'Amanda',
+      lastName: 'Simons',
     }
   ]
 
-  const initialValues: { name?: string; quantities?: string; username?: string } = {
+  const initialValues: Values  = {
     username: users[0].username
   };
 
+  const formSubmitted = ((values: Values, actions: FormikHelpers<Values>) => {
+    if (!values || !values.name || !values.username || !values.quantities) {
+      alert('Please ensure all values are populated!')
+      actions.setSubmitting(false);
+      return;
+    }
+    fetch(`http://localhost:3001/inventory/${values.name}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ quantities: values.quantities, username: values.username }),
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error()
+        }
+        else return res.json()
+      })
+      .then((data) => {
+        dispatch({
+          type: "UPDATE",
+          item: data,
+        });
+      })
+      .catch(err => alert('Error occured! Please contact support'))
+      .finally(() => actions.setSubmitting(false));
+  })
+  
   return (
     <Flex w="full">
       <Formik
         initialValues={initialValues}
-        onSubmit={(values, actions) => {
-          if (!values || !values.name || !values.username || !values.quantities) {
-            alert('Please ensure all values are populated!')
-            actions.setSubmitting(false);
-            return;
-          }
-          fetch(`http://localhost:3001/inventory/${values.name}`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ quantities: values.quantities, username: values.username }),
-          })
-            .then((res) => {
-              if (res.status !== 200) {
-                throw new Error()
-              }
-              else return res.json()
-            })
-            .then((data) => {
-              dispatch({
-                type: "UPDATE",
-                item: data,
-              });
-            })
-            .catch(err => alert('Error occured! Please contact support'))
-            .finally(() => actions.setSubmitting(false));
-        }}
+        onSubmit={formSubmitted}
       >
         {(props) => (
           <Box w="full">
@@ -86,7 +96,7 @@ export default function InventoryForm() {
                   <FormLabel>User</FormLabel>
                   <Field as="select" name="username">
                     { users.map((user, key) =>
-                      <option value={user.username} key={key}>{`${user.name} ${user.surname}`}
+                      <option value={user.username} key={key}>{`${user.firstName} ${user.lastName}`}
                       </option>
                     ) }
                   </Field>
